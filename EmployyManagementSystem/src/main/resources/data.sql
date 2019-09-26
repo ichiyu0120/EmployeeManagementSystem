ce6ea37b27eadb6ef2f4fea9b9e8a92327ce67db
yichi <yichi@DESKTOP-GO8GQNL>
2019-09-26 17:25:45 +0900

    H2用data.sql更新

--- EmployyManagementSystem/src/main/resources/data.sql
+++ EmployyManagementSystem/src/main/resources/data.sql
@@ -1,6 +1,8 @@
 /** 従業員データ**/
-INSERT INTO department (dept_id, dept_name) VALUES (1, '管理本部');
+INSERT INTO department (dept_id, dept_name) VALUES (1, '営業部');
+INSERT INTO department (dept_id, dept_name) VALUES (2, '経理部');
+INSERT INTO department (dept_id, dept_name) VALUES (3, '総務部');
 
 INSERT INTO emp (emp_id,emp_pass,emp_name,gender,address,birthday,role,dept_id,enable)
- values (1, 'password', '山田太郎', 1, '東京', '2000-11-01','ROLE_ADMIN', 1, true);
+ values (1, '$2a$10$NFsNP4211JOrYA5tpd2GHejUigEOO6K47qC8texXikLc8ZNAHIiIK', '山田太郎', 1, '東京', '2000-11-01','ROLE_ADMIN', 1, true);
 
